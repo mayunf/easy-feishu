91435f88f57c8ccf82e625a342d566ed0380416d
马运锋 <mayunfeng@jwsem.com>
2022-01-15 17:06:41 +0800

    refactor: 新增加云文档文件夹相关API

--- src/User/Drive/Drive.php
+++ src/User/Drive/Drive.php
@@ -14,6 +14,12 @@ class Drive extends AbstractAPI
     const API_POST_FILES_UPLOAD_PART = 'https://open.feishu.cn/open-apis/drive/v1/files/upload_part';
     const API_POST_FILES_UPLOAD_FINISH = 'https://open.feishu.cn/open-apis/drive/v1/files/upload_finish';
 
+    const API_POST_FOLDER_CREATE = 'https://open.feishu.cn/open-apis/drive/explorer/v2/folder/';
+    const API_GET_ROOT_FOLDER_META = 'https://open.feishu.cn/open-apis/drive/explorer/v2/root_folder/meta';
+    const API_GET_FOLDER_META = 'https://open.feishu.cn/open-apis/drive/explorer/v2/folder/%s/meta';
+    const API_GET_FOLDER_CHILDREN = 'https://open.feishu.cn/open-apis/drive/explorer/v2/folder/%s/children';
+
+
     /**
      * 上传文件.
      *
@@ -69,6 +75,56 @@ class Drive extends AbstractAPI
         return $this->parseJSON('post', [
             self::API_POST_FILES_UPLOAD_FINISH,
             $params,
+        ]);
+    }
+
+
+    /**
+     * 根据 folderToken 在该 folder 下创建文件夹
+     * @param  string  $folder  token
+     * @param  string  $title  title
+     * @return Collection
+     */
+    public function folderCreate(string $folder, string $title): Collection
+    {
+        return $this->parseJSON('post', [
+            self::API_POST_FOLDER_CREATE . $folder,
+            compact('title')
+        ]);
+    }
+    /**
+     * 获取我的空间（root folder）元信息
+     * @return Collection
+     */
+    public function rootFolderMeta(): Collection
+    {
+        return $this->parseJSON('get', [
+            self::API_GET_ROOT_FOLDER_META
+        ]);
+    }
+    /**
+     * 获取文件夹元信息
+     * @param  string  $folder  token
+     * @return Collection
+     */
+    public function folderMeta(string $folder): Collection
+    {
+        return $this->parseJSON('get', [
+            sprintf(self::API_GET_FOLDER_META,$folder)
+        ]);
+    }
+    /**
+     * 获取文件夹元信息
+     * @param  string  $folder  token
+     * @param  array  $types  types
+     * @return Collection
+     */
+    public function folderChildren(string $folder, array $types = ['doc','sheet','file','bitable','folder']): Collection
+    {
+
+        return $this->parseJSON('get', [
+            sprintf(self::API_GET_FOLDER_CHILDREN,$folder),
+            compact('types')
         ]);
     }
 
